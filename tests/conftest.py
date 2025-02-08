9057d1ededc2b3857eb3fae1397356d948e4868e
roganin <eroganin@bk.ru>
2025-02-08 16:50:41 +0300

    Добавление metadata для теста

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,20 @@
 import pytest
 
+def snake_to_pascal(snake_str):
+    # Разделяем строку по символу подчеркивания
+    components = snake_str.split('_')
+    # Преобразуем каждую часть в PascalCase (первая буква заглавная)
+    pascal_str = ''.join(component.capitalize() for component in components)
+    return pascal_str
 
 @pytest.fixture(scope='function', autouse=True)
-def test_function(record_xml_attribute):
-    record_xml_attribute("assertions", "REQ-1234")
-    record_xml_attribute("classname", "custom_classname")
-    record_xml_attribute("name", "custom_name")
-    record_xml_attribute("func", "custom_func")
+def test_function(record_xml_attribute, request):
+    parent = request.path.parts[-2: -1][0]
+    parent_str = snake_to_pascal(parent)
+
+    func = request.path.stem
+    func_str = snake_to_pascal(func)
+
+    record_xml_attribute("classname", parent_str)
     print("hello world")
     assert True
