134d83cd7256eadc1f1e7d050563cdf6f6fdbe21
roganin <eroganin@bk.ru>
2025-02-08 16:25:36 +0300

    Добавление metadata для теста

--- tests/test_connection/test_api/connection/test_read.py
+++ tests/test_connection/test_api/connection/test_read.py
@@ -1,2 +1,10 @@
+import pytest
+
+
+@pytest.mark.metadata(
+    author="John Doe",
+    description="This test checks if the login feature works correctly",
+    environment="Staging",
+)
 def test_read_json_connection():
     assert False, 'Чтение поля json_connection'
