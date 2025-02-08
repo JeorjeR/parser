a23a45700121f7443776d966070a24a2eba754c4
roganin <eroganin@bk.ru>
2025-02-08 17:17:05 +0300

    Добавление metadata для теста

---
+++ tests/test_sync/test_unit/ext_sync_doc/test_jopa1/test_jopa_inner/test_jopa.py
@@ -0,0 +1,10 @@
+from conftest import description
+
+
+@description('Запись объектов в систему')
+def test_read_json_connection():
+    """
+    Подготовка объектов
+    """
+    __doc__ = 'Подготовка объектов'
+    assert False, 'Запись объектов в систему'
