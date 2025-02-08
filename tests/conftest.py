acf0c454e87718e1c36ec23388e72f4739433a64
roganin <eroganin@bk.ru>
2025-02-08 18:02:13 +0300

    Добавление metadata для теста

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,20 @@
+import dataclasses
+import enum
+from typing import Literal
+
 import pytest
+
+
+class TestTypes(enum.StrEnum):
+    API = 'api'
+    INTEGRATION = 'integration'
+    UNIT = 'unit'
+
+@dataclasses.dataclass
+class TestDescription:
+    class_name: str
+    name: str
+
 
 @pytest.fixture(scope='function', autouse=True)
 def description(request):
@@ -18,12 +34,37 @@ def snake_to_pascal(snake_str):
 
 @pytest.fixture(scope='function', autouse=True)
 def test_function(record_xml_attribute, request):
+
+    tests_root_path_idx = request.path.parts.index('tests') + 2
+    test_type_dir_path = request.path.parts[tests_root_path_idx].split('_')[1]
+    res = TestTypes(test_type_dir_path)
+
+    if res == TestTypes.API:
+        test_description = description_api_tests(request)
+    elif res == TestTypes.UNIT:
+        test_description =  description_unit_test(request)
+    else:
+        test_description = description_integration_tests(request)
+
+    if test_description:
+        if test_description.class_name:
+            record_xml_attribute("classname", test_description.class_name)
+        if test_description.name:
+            record_xml_attribute("name", test_description.name)
+
+
+def description_api_tests(request) -> TestDescription:
     parent = request.path.parts[-2: -1][0]
     parent_str = snake_to_pascal(parent)
     name = request.node.function.__doc__.splitlines()[0].strip()
 
-    func = request.path.stem
-    func_str = snake_to_pascal(func)
+    return TestDescription(
+        class_name=parent_str,
+        name=name,
+    )
 
-    record_xml_attribute("classname", parent_str)
-    record_xml_attribute("name", name)
+def description_integration_tests(request) -> TestDescription | None:
+    ...
+
+def description_unit_test(request) -> TestDescription | None:
+    ...
