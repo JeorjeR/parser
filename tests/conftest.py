abc6c64cebededaa46a0554236a13d4f4a347d59
roganin <eroganin@bk.ru>
2025-02-08 16:42:48 +0300

    Добавление metadata для теста

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,5 +5,7 @@ import pytest
 def test_function(record_xml_attribute):
     record_xml_attribute("assertions", "REQ-1234")
     record_xml_attribute("classname", "custom_classname")
+    record_xml_attribute("name", "custom_name")
+    record_xml_attribute("func", "custom_func")
     print("hello world")
     assert True
