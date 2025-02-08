ed6641300409d911d1591c2634acd6fdd5b7c608
roganin <eroganin@bk.ru>
2025-02-08 16:26:50 +0300

    Добавление metadata для теста

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,1 @@
-import pytest
 
-
-@pytest.fixture(scope='function', autouse=True)
-def add_in_report(record_property):
-    record_property("example_key", 1)
-    record_property("example_key1", 1)
-    record_property("example_key2", 1)
